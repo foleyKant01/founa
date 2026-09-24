834fc317a2949778ba4c4876365d763e10c4c97c
foleykant <krayediego@gmail.com>
2026-09-24 17:01:15 +0000

    ok

--- founa-back/ressources/pushnotification.py
+++ founa-back/ressources/pushnotification.py
@@ -6,3 +6,8 @@ class PushNotificationApi(Resource):
     def post(self, route):
         if route == "register_device_token": 
             return RegisterDeviceToken()
+        
+        return {
+            "status": "error",
+            "message": "Route inconnue"
+        }, 404
--- founa-front/src/services/notification.service.ts
+++ founa-front/src/services/notification.service.ts
@@ -51,7 +51,7 @@ export const registerFCMToken = async (
     console.log("FCM TOKEN :", token);
 
     await axios.post(
-      `/notifications/register_device_token`,
+      `/pushnotification/register_device_token`,
       {
         user_uid,
         user_type,
